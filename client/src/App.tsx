7076d23a10f8dfa2688337e4eff50d36e7a20129
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2024-12-12 11:35:31 +0100

    🐛 BugFix - Dependance array added in the useEffect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,10 +11,10 @@ function App() {
   const [data, setData] = useState<VideoGamesI[]>([]);
 
   useEffect(() => {
-    fetch("http://localhost:3310/api/video-game")
+    fetch("http://localhost:3310/api/video-games")
       .then((res) => res.json())
       .then((videoGames) => setData(videoGames));
-  });
+  }, []);
 
   return (
     <>
