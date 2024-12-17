d2b69f3cb691bb681cbc6d8cdc5942d687027264
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2024-12-17 12:08:54 +0100

    🧩 Puzzle game - To do

--- server/src/router.ts
+++ server/src/router.ts
@@ -3,20 +3,5 @@ import express from "express";
 const router = express.Router();
 
 /* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
-
-// Define item-related routes
-import itemActions from "./modules/item/itemActions";
-
-router.get("/api/items", itemActions.browse);
-router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
-
-/* ************************************************************************* */
-
-import videoGamesActions from "./modules/videoGames/videoGamesActions";
-
-router.get("/api/video-games", videoGamesActions.browse);
 
 export default router;
